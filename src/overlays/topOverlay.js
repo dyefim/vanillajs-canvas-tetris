c683fff84dc02cd1d08d6a99eca659d2b9af3fe9
dyefim <yefimchuk.dmytro@gmail.com>
2021-07-02 14:01:36 +0300

    fix score size

--- src/overlays/topOverlay.js
+++ src/overlays/topOverlay.js
@@ -8,7 +8,7 @@ const gradient = context.createLinearGradient(0, 0, overlayCanvas.width, 0);
 gradient.addColorStop('0', '#e47');
 gradient.addColorStop('1.0', '#ff0');
 
-const scoreFontSize = overlayCanvas.height / 2;
+const scoreFontSize = overlayCanvas.height * 0.65;
 
 context.font = `${scoreFontSize}px sans-serif`;
 context.fillStyle = gradient;
@@ -17,7 +17,7 @@ const drawScore = () => {
   context.fillText(
     `Score: ${score.points}`,
     10,
-    (overlayCanvas.height + scoreFontSize) / 2
+    (overlayCanvas.height - scoreFontSize * 1.33) / 2 + scoreFontSize
   );
 };
 
